22ad295e26e06f41643f350bd5d3abec79c7a50c
WoutBeeckman <119806930+WoutBeeckman@users.noreply.github.com>
2026-01-06 11:11:28 +0100

    Add Introductie_CSharp solution

---
+++ Introductie_CSharp/Introductie_CSharp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Introductie_C_.HelperLib;
+
+internal class Program
+{
+    static void Main()
+    {
+        Helper.makeCammelCase("dit is een test zin");
+
+
+        //int a = 10;
+        //int b = 20;
+
+        //Helper.Wissel(a, b);
+
+        //Console.WriteLine($"a: {a}, b: {b}");
+    }
+}
---
+++ Introductie_CSharp/Introductie_CSharp/HelperLib/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Introductie_C_.HelperLib;
+
+public static class Helper
+{
+    public static void Wissel(int a, int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void makeCammelCase(string Zin)
+    {
+        //  Zin splitsen in woorden
+        string[] woorden = Zin.Split(' ');
+
+        // Woorden met hoofdletter schrijven
+        for (int i = 0; i < woorden.Length; i++)
+        {
+            if (woorden[i].Length > 0)
+            {
+                woorden[i] = char.ToUpper(woorden[i][0]) + woorden[i].Substring(1).ToLower();
+            }
+        }   
+
+        Console.WriteLine(woorden);
+    }
+    public static void Maand()
+    {
+
+    }
+
+}
